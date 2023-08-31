d4cffee56d9896cfa368c4120f7333dcd71c8dd7
Kehinde Adedokun <75108469+hi-heavens@users.noreply.github.com>
2023-08-31 09:00:54 +0100

    refactor: import the generateAccountNumber in the model

--- src/models/customer.model.js
+++ src/models/customer.model.js
@@ -1,14 +1,12 @@
+const generateAccountNumber = require("../services/generateAccountNumber");
+
 class BankAccount {
   constructor(holderName, dob, accountType, initialBalance) {
-    this.accountNumber = this.generateAccountNumber();
+    this.accountNumber = generateAccountNumber();
     this.holderName = holderName;
     this.dob = dob;
     this.accountType = accountType;
     this.balance = initialBalance;
-  }
-
-  generateAccountNumber() {
-    return Math.floor(1000000000 + Math.random() * 9000000000);
   }
 }
 
